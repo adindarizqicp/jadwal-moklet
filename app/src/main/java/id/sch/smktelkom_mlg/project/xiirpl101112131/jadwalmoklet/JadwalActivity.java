f886f916d5a2521e30051d8c4c26b74eedffdbb6
Adinda Rizqi <adindarizqicaesariaputri@gmail.com>
2016-10-13 16:24:08 -0800

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/JadwalActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class JadwalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal);
+    }
+}
