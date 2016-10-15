246f2ff1c28aae7b59a9a6900c05bffb6c7db14f
bungasav <bungapramestis86@gmail.com>
2016-10-15 12:40:10 +0700

    membuat viewpager untuk slider

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/Jad3.java
@@ -0,0 +1,18 @@
+package id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Jad3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (container == null) {
+            return null;
+        }
+        return inflater.inflate(R.layout.jad3_layout, container, false);
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/PagerAdapter.java
@@ -0,0 +1,31 @@
+package id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet;
+
+/**
+ * Created by Bunga on 15/10/2016.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragments;
+
+    public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+        return this.fragments.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+}
