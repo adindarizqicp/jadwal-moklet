b756fd38d23fdf54941a3a34fb0dd061682abb83
bungasav <bungapramestis86@gmail.com>
2016-10-22 14:27:52 +0700

    slider tampilan list jadwal

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/SampleFragmentPagerAdapter.java
@@ -0,0 +1,30 @@
+package id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 5;
+    private String tabTitles[] = new String[]{"Tab1", "Tab2", "Tab3", "Tab4", "Tab5"};
+
+    public SampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return Page_Fragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
