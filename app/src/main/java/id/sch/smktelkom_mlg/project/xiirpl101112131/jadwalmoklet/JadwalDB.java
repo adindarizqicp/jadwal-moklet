05782b6c65754cecbed040b954e51ce0eb68715a
Adinda Rizqi <adindarizqicaesariaputri@gmail.com>
2016-11-05 18:17:53 -0800

    Konversi data (belum  di tes)

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/JadwalDB.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/JadwalDB.java
@@ -1,22 +1,22 @@
 package id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.SQLException;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Map;
 
-import id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet.SQLite.DBhelper;
 import id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet.SQLite.SQLController;
 
 /**
@@ -24,11 +24,12 @@ import id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet.SQLite.SQLContr
  */
 
 public class JadwalDB extends AppCompatActivity {
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("XIIRPL1");
+    FirebaseDatabase fireDB = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = fireDB.getReference("XIIRPL1");
 
     ArrayList<String> myList = new ArrayList<String>();
     ArrayList<String> myListSenin;
+    String[] myJ, myJ_C;
     Context myContext;
     private SQLController dbController;
 
@@ -42,81 +43,129 @@ public class JadwalDB extends AppCompatActivity {
     }
 
     public ArrayList DBSeninToArray() {
-        Cursor cursor = dbController.getDB().rawQuery("select * from myJadwal_out where Hari = \"Senin001\" ", null);
-        String[] from = new String[]{
-                DBhelper.HARI,
-                DBhelper.JP_01,
-                DBhelper.JP_02,
-                DBhelper.JP_03,
-                DBhelper.JP_04,
-                DBhelper.JP_05,
-                DBhelper.JP_06,
-                DBhelper.JP_07,
-                DBhelper.JP_08,
-                DBhelper.JP_09,
-                DBhelper.JP_10,
-                DBhelper.JP_11,
-                DBhelper.JP_12};
+        try {
+            Cursor cursor = dbController.getDB().rawQuery("select * from myJadwal_out where Hari = \"Senin001\" ", null);
 
-        /*ArrayList<String> mArrayList = new ArrayList<String>();
-        if(cursor.moveToFirst()){
-            do{
-                mArrayList.add(cursor.getString(cursor.getColumnIndex("Senin001"))); //add the item
-            }while(cursor.moveToNext());
-        }*/
+            ArrayList<String> maplist = new ArrayList<String>();  // looping through all rows and adding to list
 
-        ArrayList<String> maplist = new ArrayList<String>();  // looping through all rows and adding to list
+            if (cursor.moveToFirst()) {
+                do {
+                    for (int i = 1; i < cursor.getColumnCount(); i++) {
+                        maplist.add(cursor.getString(i));
+                    }
+                } while (cursor.moveToNext());
+            }
 
-        if (cursor.moveToFirst()) {
-            do {
-                for (int i = 1; i < cursor.getColumnCount(); i++) {
-                    maplist.add(cursor.getString(i));
-                }
-            } while (cursor.moveToNext());
+            cursor.close();
+
+            return maplist;
+        } catch (SQLException e) {
+            System.out.println("OYEE " + e.toString());
+            ArrayList<String> maplist = new ArrayList<String>();
+            maplist.add(" - ");
+            return maplist;
         }
-
-        cursor.close();
-
-        return maplist;
     }
 
 
     public void getSenin() {
         myRef.child("Senin").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(DataSnapshot dataSnapshot) { //AMBIL DATA DARI FIREBASE------------
                 MataPelajaran mataPelajaran = dataSnapshot.getValue(MataPelajaran.class);
-                try {
+
+                try { //HAPUS RECORD YG SDH ADA
                     dbController.delete("Senin001");
                 } catch (SQLException e) {
-
+                    System.out.println("BEH! " + e.toString());
                 }
 
+                //AMBIL DATA DARI FIREBASE (disimpan ke array)------------
+                myJ = new String[]{
+                        mataPelajaran.getJ1(),
+                        mataPelajaran.getJ2(),
+                        mataPelajaran.getJ3(),
+                        mataPelajaran.getJ4(),
+                        mataPelajaran.getJ5(),
+                        mataPelajaran.getJ6(),
+                        mataPelajaran.getJ7(),
+                        mataPelajaran.getJ8(),
+                        mataPelajaran.getJ9(),
+                        mataPelajaran.getJ10(),
+                        mataPelajaran.getJ11(),
+                        mataPelajaran.getJ12()
+                };
 
+                //KONVERSI DATA--------------------------
+                myJ_C = new String[12];
+                try {
+                    for (int i = 0; i < myJ.length; i++) {
+                        Query queryRef = myRef.orderByChild(myJ[i]);
+                        final int finalI = i;
+                        queryRef.addChildEventListener(new ChildEventListener() {
+                            @Override
+                            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                                Map<String, Object> value = (Map<String, Object>) dataSnapshot.getValue();
+                                String myKelas = "XIIRPL";
+                                myJ_C[finalI] = String.valueOf(value.get(myKelas));
+                                if (myJ_C[finalI].isEmpty())
+                                    myJ_C[finalI] = " - ";
+                            }
+
+                            @Override
+                            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                            }
+
+                            @Override
+                            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                            }
+
+                            @Override
+                            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+
+                } catch (SQLException e) {
+                    System.out.println("BEH! " + e.toString());
+                }
+
+                //INSERT KE SQLITE---------------------------
                 try {
                     dbController.insert(
                             "Senin001",
-                            mataPelajaran.getJ1(),
-                            mataPelajaran.getJ2(),
-                            mataPelajaran.getJ3(),
-                            mataPelajaran.getJ4(),
-                            mataPelajaran.getJ5(),
-                            mataPelajaran.getJ6(),
-                            mataPelajaran.getJ7(),
-                            mataPelajaran.getJ8(),
-                            mataPelajaran.getJ9(),
-                            mataPelajaran.getJ10(),
-                            mataPelajaran.getJ11(),
-                            mataPelajaran.getJ12()
+                            myJ_C[0],
+                            myJ_C[1],
+                            myJ_C[2],
+                            myJ_C[3],
+                            myJ_C[4],
+                            myJ_C[5],
+                            myJ_C[6],
+                            myJ_C[7],
+                            myJ_C[8],
+                            myJ_C[9],
+                            myJ_C[10],
+                            myJ_C[11]
                     );
-                } catch (SQLException e) {
 
+                    message("Info", "Berhasil memperbarui jadwal");
+
+                } catch (SQLException e) {
+                    System.out.println("BEH! " + e.toString());
                 }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                error("Gagal melakukan koneksi");
+                message("Alert", "Gagal melakukan koneksi");
             }
         });
     }
@@ -140,14 +189,14 @@ public class JadwalDB extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                error("Gagal melakukan koneksi");
+                message("Alert", "Gagal melakukan koneksi");
             }
         });
     }
 
-    public void error(String msg) {
-        AlertDialog alertDialog = new AlertDialog.Builder(JadwalDB.this).create();
-        alertDialog.setTitle("Alert");
+    public void message(String jdl, String msg) {
+        /*AlertDialog alertDialog = new AlertDialog.Builder().create();
+        alertDialog.setTitle(jdl);
         alertDialog.setMessage(msg);
         alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                 new DialogInterface.OnClickListener() {
@@ -155,7 +204,7 @@ public class JadwalDB extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
-        alertDialog.show();
+        alertDialog.show();*/
     }
 
 
