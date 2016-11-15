2e307d27e25d69bc91a4f3b8cb1d362fd821471e
bungasav <bungapramestis86@gmail.com>
2016-11-15 09:55:50 +0700

    Pop up, coming soon, exit, dan layout jadwal

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/InputActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl101112131/jadwalmoklet/InputActivity.java
@@ -1,9 +1,11 @@
 package id.sch.smktelkom_mlg.project.xiirpl101112131.jadwalmoklet;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Spinner;
@@ -28,7 +30,34 @@ public class InputActivity extends AppCompatActivity {
             Intent intent = new Intent(InputActivity.this, MainActivity.class);
             startActivity(intent);
         }
+        findViewById(R.id.textViewExit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                metu();
+            }
+        });
 
+    }
+
+    private void metu() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case DialogInterface.BUTTON_POSITIVE:
+
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        startActivity(new Intent(InputActivity.this, activity_jadwal.class));
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Yakin keluar?").setNegativeButton("Yes", dialogClickListener)
+                .setPositiveButton("No", dialogClickListener).show();
     }
 
     public void Save(View view) {
